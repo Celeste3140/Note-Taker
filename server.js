43f0be37e33dd2dfe661bba9a554f90f7e60fdd7
carambola <celestebaltazar@yahoo.com>
2022-08-03 21:33:40 -0700

    set up express app, refrenced code from activity 09-Ins_Static-Assets

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const express = require("require");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
 );
