bab625161356bbaca0c4e02d7bc3488223adfa6a
carambola <celestebaltazar@yahoo.com>
2022-08-03 21:26:30 -0700

    added express var and listener

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const fs = require("fs");
 const path = require ("path");
 const express = require("require");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.listen(PORT, () =>
+  console.log(`Express server listening on port ${PORT}!`)
+);
